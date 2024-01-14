911979ee832f65fd7da5751a543516f095149d95
alivelusai <93896926+alivelusai@users.noreply.github.com>
2024-01-14 17:22:07 +0530

    Update deploy.sh

--- kafka/deploy.sh
+++ kafka/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
+sudo ./aws/install
+aws --version
 kubectl create  ns kafka
 kubectl apply -k ./
